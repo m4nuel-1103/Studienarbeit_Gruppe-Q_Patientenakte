781a20219940b8f9ef7b5b48b62e0d07a45c9b80
Roland Ambs <roland.ambs@meteoblue.com>
2025-03-18 11:10:29 +0100

    change start script

--- start.sh
+++ start.sh
@@ -1,16 +1,17 @@
-docker compose up
+#!/bin/sh
+docker compose start || docker compose up -d
 cd ./contracts-test || exit
 npm install .
-npx hardhat node
+npx hardhat node &
 npx hardhat run --network localhost scripts/deploy.js
 cd ..
 cd ./backend || exit
 npm install .
 npx drizzle-kit generate
 npx drizzle-kit push --url postgresql://backu:example@localhost/app  --dialect postgresql --schema db/schema.ts
-npm run dev
+npm run dev &
 cd ..
 cd ./frontend/patientenakte_anwendung || exit
 npm install .
-npm run dev
+npm run dev &
 cd ../..
