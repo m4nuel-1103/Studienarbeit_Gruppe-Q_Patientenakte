7068d685429a7b155dac459d1b94eff430e21df6
Roland Ambs <roland.ambs@meteoblue.com>
2025-03-18 10:03:42 +0100

    correct logic

--- frontend/patientenakte_anwendung/src/Pages/Doctor/PatientsDetails.tsx
+++ frontend/patientenakte_anwendung/src/Pages/Doctor/PatientsDetails.tsx
@@ -58,9 +58,7 @@ const PatientsDetails = (props: AddressProps) => {
             console.log(contract);
             const enc = new TextEncoder();
             if (!contract) { console.error("no contract"); return; }
-            console.log("doc-id: ", doc.documentId);
             const hasAc: { access: boolean, expiresAt: bigint, remainingUses: bigint } = await contract.hasAccess(BigInt(doc.documentId));
-            console.log(hasAc);
             if (!hasAc.access) {
                 window.alert("Ihr Zugriff auf dieses Dokument ist abgelaufen");
                 const resp = await (await fetch(`/api/released_documents/${doc.id}`, { method: "DELETE" })).json();
@@ -102,7 +100,7 @@ const PatientsDetails = (props: AddressProps) => {
             const blob = new Blob([byteArray], { type: 'application/pdf' });
             const url = URL.createObjectURL(blob);
             setPdfUrl(url);
-            if (hasAc.remainingUses <= 1) {
+            if (hasAc.remainingUses == 1n) {
                 const resp = await (await fetch(`/api/released_documents/${doc.id}`, { method: "DELETE" })).json();
                 console.log(`deleted document ${doc.id} (${resp})`);
                 return;
