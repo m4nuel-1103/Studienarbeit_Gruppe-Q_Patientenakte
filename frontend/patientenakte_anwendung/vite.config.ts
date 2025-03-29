69732a2afd5bb0d0f788b1a14e07c204ef0c0c9f
Roland Ambs <roland.ambs@meteoblue.com>
2025-03-29 09:58:03 +0100

    fix potential error

--- frontend/patientenakte_anwendung/vite.config.ts
+++ frontend/patientenakte_anwendung/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
             // port: 3000,
             proxy: {
                 "/api": {
-                    target: "http://127.0.0.1:3000",
+                    target: "http://localhost:3000",
                     changeOrigin: true,
                     secure: false,
                     rewrite: (path) => path.replace(/^\/api/, ""),
